b24754f16b8743b676b66edabfb32a1dd9a1be32
Ryan B <bombingrun3@gmail.com>
2025-06-28 17:05:59 -0400

    Added Fonts and Styled Header and Home Roughly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,19 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <p>This is the Header</p>
+      <div className="header__logo"></div>
+      <div className="header__menu">
+        {/* Make NavBar Component vvv */}
+        <div className="navbar">
+          <p className="navbar__link">Recipes</p>
+          <p className="navbar__link">About</p>
+        </div>
+        <div className="header__user-info">
+          <p className="header__username">Ryan Bommarito</p>
+          {/* Make avatar an image tag vvv */}
+          <div className="header__avatar"></div>
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,14 @@ import "./Home.css";
 
 function Home() {
   return (
-    <section>
-      <p>Food Finder</p>
+    <section className="home">
+      <h1 className="home__title">Food Finder</h1>
+      <div className="home__subtext-container">
+        <p className="home__subtext">FIND A RECIPE</p>
+        <p className="home__subtext">ORDER THE INGREDIENTS</p>
+        <p className="home__subtext">ENJOY!</p>
+      </div>
+      <button className="home__button">Get Started</button>
     </section>
   );
 }
