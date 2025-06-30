349f26b78db2b43b331d77efd13322732453a9b7
Ryan B <bombingrun3@gmail.com>
2025-06-30 00:59:30 -0400

    Added HTML and Styled Profile Settings Component

--- src/components/ProfileSettings/ProfileSettings.jsx
+++ src/components/ProfileSettings/ProfileSettings.jsx
@@ -2,9 +2,30 @@ import "./ProfileSettings.css";
 
 function ProfileSettings() {
   return (
-    <>
-      <h2>PROFILE SETTINGS</h2>
-    </>
+    <section className="profile-settings">
+      <h2 className="profile-settings__title">UPDATE USER INFO</h2>
+      <form className="profile-settings__form">
+        <label className="profile-settings__label">
+          Username
+          <input
+            className="profile-settings__input"
+            type="text"
+            placeholder="Username"
+          ></input>
+        </label>
+        <label className="profile-settings__label">
+          Avatar Image Url
+          <input
+            className="profile-settings__input"
+            type="Url"
+            placeholder="Image Url"
+          ></input>
+        </label>
+        <button className="profile-settings__submit-button" type="button">
+          SAVE
+        </button>
+      </form>
+    </section>
   );
 }
 
