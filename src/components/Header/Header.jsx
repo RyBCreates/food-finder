6ddaf187db568a8ad2d625b2c073914232b66608
Ryan B <bombingrun3@gmail.com>
2025-07-07 13:11:51 -0500

    Fixed Header User Info to reflect the Current User data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import defaultAvatar from "../../assets/default-avatar.svg";
 import Navbar from "../Navbar/Navbar";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./Header.css";
 
 function Header() {
+  const { currentUser } = useContext(CurrentUserContext);
   return (
     <header className="header">
       <Link to="/">
@@ -12,12 +15,12 @@ function Header() {
       <div className="header__menu">
         <Navbar />
         <Link className="header__user-info" to="/profile">
-          <p className="header__username">Ryan Bommarito</p>
+          <p className="header__username">{currentUser?.name}</p>
           <div className="header__avatar-wrapper">
             <img
               className="header__avatar"
-              src={defaultAvatar}
-              alt="default avatar"
+              src={currentUser?.avatar || defaultAvatar}
+              alt={currentUser?.name || "default avatar"}
             />
           </div>
         </Link>
