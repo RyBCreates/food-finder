83c6c8d0be3b37548a7255e14ccbac1b9c08dd01
Ryan B <bombingrun3@gmail.com>
2025-07-08 11:19:39 -0500

    Added a like button to RecipeCard

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -6,7 +6,10 @@ function RecipeCard({ recipe, onCardClick }) {
     <li className="card" onClick={() => onCardClick(recipe)}>
       <img className="card__image" src={recipe.image} alt={recipe.title} />
       <div className="card__info">
-        <h3 className="card__title">{recipe.title}</h3>
+        <div className="card__header">
+          <button className="card__like-button" type="button"></button>
+          <h3 className="card__title">{recipe.title}</h3>
+        </div>
         <div className="card__recipe-stats">
           <p className="card__stat">
             Prep Time:
