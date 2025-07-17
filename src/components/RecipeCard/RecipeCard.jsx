2b79c11d863456f9a9868ddcae2400617728e021
Ryan B <bombingrun3@gmail.com>
2025-07-17 15:13:52 -0500

    Stopped Propagation on the RecipeCards like button

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -2,13 +2,20 @@ import { formatPrice } from "../../utils/formatPrice";
 import unliked from "../../assets/like-button/default-like.svg";
 import "./RecipeCard.css";
 
-function RecipeCard({ recipe, onCardClick }) {
+function RecipeCard({ recipe, onCardClick, handleAddFavoriteRecipe }) {
   return (
     <li className="card" onClick={() => onCardClick(recipe)}>
       <img className="card__image" src={recipe.image} alt={recipe.title} />
       <div className="card__info">
         <div className="card__header">
-          <button className="card__like-button" type="button">
+          <button
+            className="card__like-button"
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleAddFavoriteRecipe(recipe);
+            }}
+          >
             <img
               className="card__like-icon"
               src={unliked}
