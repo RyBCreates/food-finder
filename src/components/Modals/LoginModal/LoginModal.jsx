8bffe7bbc128b90a7c8f3648f672d678265de52e
Ryan B <bombingrun3@gmail.com>
2025-08-11 17:05:03 -0400

    Added Close functions to auth Modals

--- src/components/Modals/LoginModal/LoginModal.jsx
+++ src/components/Modals/LoginModal/LoginModal.jsx
@@ -1,10 +1,18 @@
+import Close from "../../../assets/close-button.svg";
 import "./LoginModal.css";
 import "../Modals.css";
 
-function LoginModal({ activeModal }) {
+function LoginModal({ activeModal, closeModal }) {
   return (
     <div className={`modal ${activeModal === "login" && "modal_opened"}`}>
       <div className="modal__content">
+        <button
+          className="modal__close-button"
+          type="button"
+          onClick={closeModal}
+        >
+          <img className="modal__close-icon" src={Close} alt="close icon" />
+        </button>
         <h2>This is the Login Modal</h2>
       </div>
     </div>
--- src/components/Modals/RegisterModal/RegisterModal.jsx
+++ src/components/Modals/RegisterModal/RegisterModal.jsx
@@ -1,10 +1,18 @@
+import Close from "../../../assets/close-button.svg";
 import "./RegisterModal.css";
 import "../Modals.css";
 
-function RegisterModal({ activeModal }) {
+function RegisterModal({ activeModal, closeModal }) {
   return (
     <div className={`modal ${activeModal === "register" && "modal_opened"}`}>
       <div className="modal__content">
+        <button
+          className="modal__close-button"
+          type="button"
+          onClick={closeModal}
+        >
+          <img className="modal__close-icon" src={Close} alt="close icon" />
+        </button>
         <h2>This is the Login Modal</h2>
       </div>
     </div>
